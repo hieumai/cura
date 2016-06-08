e0fd8f571d52d459c3617ca8a8d6cc7608729628
vtnlinh95 <jennylinh95@gmail.com>
2016-06-08 17:04:26 +0700

    KATI-13_Patient-Profile-View-Feature add loadData

--- app/src/main/java/com/kms/cura/view/PatientProfileViewActivity.java
+++ app/src/main/java/com/kms/cura/view/PatientProfileViewActivity.java
@@ -6,17 +6,23 @@ import android.support.v7.widget.Toolbar;
 import android.view.Gravity;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import com.kms.cura.R;
+import com.kms.cura.entity.user.PatientUserEntity;
+import com.kms.cura.utils.PatinetProfileUtils;
 
 public class PatientProfileViewActivity extends AppCompatActivity {
     private Toolbar tbPatientProfileView;
+    private TextView txtName, txtGender, txtDOB, txtLocation, txtInsurance;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_patient_profile_view);
         initToolBar();
+        initView();
+        loadData();
     }
 
     public void initToolBar() {
@@ -27,4 +33,21 @@ public class PatientProfileViewActivity extends AppCompatActivity {
         Toolbar.LayoutParams layoutParams = new Toolbar.LayoutParams(Gravity.CENTER_HORIZONTAL);
         tbPatientProfileView.addView(content_toolbar, layoutParams);
     }
+
+    public void initView() {
+        txtName = (TextView) findViewById(R.id.txtName);
+        txtGender = (TextView) findViewById(R.id.txtGender);
+        txtDOB = (TextView) findViewById(R.id.txtDOB);
+        txtLocation = (TextView) findViewById(R.id.txtLocation);
+        txtInsurance = (TextView) findViewById(R.id.txtInsurance);
+    }
+    public void loadData(){
+        PatientUserEntity entity = PatinetProfileUtils.getInstance().getData();
+        txtName.setText(entity.getName());
+        txtGender.setText(entity.getGender());
+        txtDOB.setText(entity.getBirth().toString());
+        txtLocation.setText(entity.getLocation());
+        txtInsurance.setText(entity.getInsurance());
+    }
+
 }
