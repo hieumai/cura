b562cb87dcbd12021e5c0b4310c7006bd7da5a41
vtnlinh95 <jennylinh95@gmail.com>
2016-06-15 11:48:54 +0700

    KATI_14_Doctor-Profile-View-Feature

--- com.kms.cura_server/src/com/kms/cura_server/SpecialityAPI.java
+++ com.kms.cura_server/src/com/kms/cura_server/SpecialityAPI.java
@@ -16,6 +16,7 @@ import com.kms.cura.dal.database.SpecialityDatabaseHelper;
 import com.kms.cura.dal.mapping.DoctorColumn;
 import com.kms.cura.entity.Entity;
 import com.kms.cura.entity.json.EntityToJsonConverter;
+import com.kms.cura.entity.user.UserEntity;
 import com.kms.cura_server.resources.Strings;
 
 @Path("/speciality")
@@ -32,17 +33,28 @@ public class SpecialityAPI {
 		}
 	}
 	@POST
-	@Path("/getbyDoctorID");
+	@Path("/getbyDoctorID")
 	public String getbyDoctorID(String jsonData) {
 		try {
 			JSONObject jsonObject = new JSONObject(jsonData);
-			int id = jsonObject.getInt(DoctorColumn.USER_ID.getColumnName());
+			int id = (int) jsonObject.get(DoctorColumn.USER_ID.getColumnName());
 			List<Entity> speciality = SpecialityDAL.getInstance().getbyDoctorID(id);
-			JsonElement element = EntityToJsonConverter.convertEntityListToJson(speciality);
-			return element.toString();
+			return SuccessResponse(speciality);
 		} catch (ClassNotFoundException | SQLException e) {
-			return Strings.error_internal;
+			return UnsuccessResponse(e.getMessage());
 		}
 		
+	}
+	private String SuccessResponse(List<Entity> specialities){
+		JsonElement jsonSpecialities = EntityToJsonConverter.convertEntityListToJson(specialities);
+		JsonObject JsonUserConvert =(JsonObject) jsonSpecialities;
+		JsonUserConvert.addProperty(UserEntity.STATUS_KEY, true);
+		return JsonUserConvert.toString();
+	}
+	private String UnsuccessResponse(String message){
+		JsonObject jsonError=new JsonObject();
+		jsonError.addProperty(UserEntity.STATUS_KEY, false);
+		jsonError.addProperty(UserEntity.MESSAGE, message);
+		return jsonError.toString();
 	}
 }
