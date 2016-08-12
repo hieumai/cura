f38ec5e3a7ca28c8d73f623714b3ed6a0073a19f
vtnlinh95 <jennylinh95@gmail.com>
2016-08-12 17:26:30 +0700

    KATI-175_Adding-new-column-id

--- com.kms.cura.dal/resources/SQL Script/Appointment.sql
+++ com.kms.cura.dal/resources/SQL Script/Appointment.sql
@@ -1,6 +1,7 @@
 USE cura;
 
 CREATE TABLE Appointments (
+	id INT NOT NULL AUTO_INCREMENT,
 	doctor_id INT NOT NULL,
     patient_id INT NOT NULL,
     facility_id INT NOT NULL,
@@ -12,7 +13,8 @@ CREATE TABLE Appointments (
     doctor_cmt text,
     FOREIGN KEY (doctor_id) REFERENCES doctor_facilities (doctor_id),
     FOREIGN KEY (patient_id) REFERENCES patient (user_id),
-    FOREIGN KEY (facility_id) REFERENCES doctor_facilities (facility_id)
+    FOREIGN KEY (facility_id) REFERENCES doctor_facilities (facility_id),
+    PRIMARY KEY (id)
 );
 
 INSERT INTO Appointments (doctor_id, patient_id, facility_id, appt_day, start_time, end_time, current_status) VALUES (3, 2, 8, 20160830, '13:00:00', '14:00:00', 0);
